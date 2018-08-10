1937937a4c1b4f08bdfdea466d7c918f77ce61d2
deepakkaushal <dkumar19@gmail.com>
2018-08-10 19:31:45 +0530

    Add files via upload

---
+++ Case1PO.java
@@ -0,0 +1,101 @@
+package pageObject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class Case1PO {
+	
+	
+	//---Registration
+	
+	public final static String  css_AccountLink= ".Popup.CustomerPopup";
+	public final static String  css_SignUpButton= ".SignInForm-signUpButton";
+	
+	public final static String  css_FirstNameTextField= ".Profile-firstName";
+	public final static String  css_LastNameTextField= ".Profile-lastName";
+	public final static String  css_EmailTextField= ".Profile-email";
+	public final static String  css_PasswordTextField= ".Profile-password.no-tracking";
+	public final static String  css_PhoneNumber= ".Profile-phoneNumber";
+	public final static String  css_SubscribeCheckBox= "#Profile-subscribe";
+	public final static String  css_ProfileSignUpButton= ".Profile-signUpButton";
+	
+	
+	public final static String  xpath_MyAccountLink= ".//*[@class='CustomerPopup-loggedIn']/a[text()='My Account']";
+	public final static String  linkText_YourDetailEditLink= "Edit";
+	public final static String  css_UpdateDetailButton= ".Profile-updateDetailsButton";
+	
+	
+	
+	
+	/**
+	 * these are the Page Factory element locators
+	 */
+	/*@FindBy(how = How.CSS, using = css_AccountLink)
+	public static WebElement CSS_AccountLink;
+	
+	@FindBy(how = How.CSS, using = css_SignUpButton)
+	public static WebElement CSS_SignUpButton;
+	
+	@FindBy(how = How.CSS, using = css_FirstNameTextField)
+	public static WebElement CSS_FirstNameTextField;
+	
+	@FindBy(how = How.CSS, using = css_LastNameTextField)
+	public static WebElement CSS_LastNameTextField;
+	
+	@FindBy(how = How.CSS, using = css_EmailTextField)
+	public static WebElement CSS_EmailTextField;
+	
+	@FindBy(how = How.CSS, using = css_PasswordTextField)
+	public static WebElement CSS_PasswordTextField;
+	
+	@FindBy(how = How.CSS, using = css_PhoneNumber)
+	public static WebElement CSS_PhoneNumber;
+	
+	@FindBy(how = How.CSS, using = css_SubscribeCheckBox)
+	public static WebElement CSS_SubscribeCheckBox;
+	
+	@FindBy(how = How.CSS, using = css_ProfileSignUpButton)
+	public static WebElement CSS_ProfileSignUpButton;
+	
+	@FindBy(how = How.XPATH, using = xpath_MyAccountLink)
+	public static WebElement XPATH_MyAccountLink;
+	
+	@FindBy(how = How.LINK_TEXT, using = linkText_YourDetailEditLink)
+	public static WebElement LINKTEXT_YourDetailEditLink;
+	
+	@FindBy(how = How.CSS, using = css_UpdateDetailButton)
+	public static WebElement CSS_UpdateDetailButton;
+	
+	@FindBy(how = How.CSS, using = css_UpdationSuccessMsg)
+	public static WebElement CSS_UpdationSuccessMsg;
+	
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+
+
+
+
+
+
+
